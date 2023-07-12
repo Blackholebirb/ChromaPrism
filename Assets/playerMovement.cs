989af4a243223f98c1baeaee4a945ae81db120e6
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2023-07-12 00:38:18 -0500

    Improve collision logic

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    private enum CollisionDirection
+    {
+        Up      = 0,
+        Down    = 1,
+        Left    = 2,
+        Right   = 3
+    }
+
     float xVel = 0, yVel = 0;
     float maxXSpeed = 0.3f;
     float xAccelFactor = 0.5f;
@@ -12,14 +20,54 @@ public class playerMovement : MonoBehaviour
     float holdJumpHeight = 10;
     bool isGrounded = true;
 
+    // TODO: This could probably use some further refinement because it's still a bit buggy
+    private CollisionDirection CheckCollisionDirection(Collider2D worldObj)
+    {
+        Vector2 contactPoint = worldObj.ClosestPoint(transform.position);
+        Vector2 center = worldObj.bounds.center;
+        float xMin = center.x - worldObj.bounds.size.x / 2;
+        float xMax = center.x + worldObj.bounds.size.x / 2;
+        float yMin = center.y - worldObj.bounds.size.y / 2;
+        float yMax = center.y + worldObj.bounds.size.y / 2;
+
+        float collsionThickness = 0.05f;
+        if (Mathf.Abs(yMax - contactPoint.y) < collsionThickness)
+            return CollisionDirection.Down;
+        else if (Mathf.Abs(yMin - contactPoint.y) < collsionThickness)
+            return CollisionDirection.Up;
+        else if (Mathf.Abs(xMin - contactPoint.x) < collsionThickness)
+            return CollisionDirection.Right;
+        else
+            return CollisionDirection.Left;
+    }
+
     // In order to get collision to work properly, the two colliding objects need to have Rigidbody 2D,
     // must be simulated, gravity scale 0, and one of them needs to have isTrigger = true
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D worldObj)
     {
-        isGrounded = true;
-        transform.Translate(0, -yVel, 0);
-        yVel = 0f;
+        CollisionDirection dir = CheckCollisionDirection(worldObj);
+        switch (dir)
+        {
+            case CollisionDirection.Down:
+                // Bottom: Cancel all vertical movement so gravity doesn't pull through the ground
+                isGrounded = true;
+                transform.Translate(0, -yVel, 0);
+                yVel = 0;
+                break;
+            case CollisionDirection.Up:
+                // Top: Bounce back with 20% speed
+                transform.Translate(0, -yVel, 0);
+                yVel = -0.2f * yVel;
+                break;
+            case CollisionDirection.Left:
+            case CollisionDirection.Right:
+                // Sides: Cancel all horizontal movement
+                transform.Translate(-xVel, 0, 0);
+                xVel = 0;
+                break;
+        }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         isGrounded = false;
